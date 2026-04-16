0025467062a2ee3d8c9a9782372168b43196741f
zaidon11 <zaidrodhanali@gmail.com>
2026-04-16 14:27:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBpt495yhuoTbjunCLIFCF_8c1NesxZHWs",
-    authDomain: "shopweb-3466b.firebaseapp.com",
-    databaseURL: "https://shopweb-3466b-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "shopweb-3466b",
-    storageBucket: "shopweb-3466b.firebasestorage.app",
-    messagingSenderId: "41811528812",
-    appId: "1:41811528812:web:618f240f8106f218a8dc49"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
 
